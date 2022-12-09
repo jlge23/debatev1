bcba5f2c32c3e2f9b3358e13a383115f941adfcd
jlge23 <49133249+jlge23@users.noreply.github.com>
2022-12-09 16:21:17 -0400

    Update Informes.js DataTables

--- resources/js/informes.js
+++ resources/js/informes.js
@@ -14,35 +14,9 @@ $(document).ready(function(){
    //modal.show();
    //Datatables resultados
    var table = $("table#DT_informe").DataTable({
-      //responsive: true,
-      //scrollX: false,
+      responsive: true,
       scrollY: '450px',
-      scrollCollapse: true,
-      //paging: false,
-      autoWidth: false,
-      fixedHeader: true,
-      initComplete: function () {
-         this.api()
-         .columns([0,1,2,3,4])
-         .every(function () {
-            var column = this;
-            var select = $('<select><option value=""></option></select>')
-                  .appendTo($(column.footer()).empty())
-                  .on('change', function () {
-                     var val = $.fn.dataTable.util.escapeRegex($(this).val());
-
-                     column.search(val ? '^' + val + '$' : '', true, false).draw();
-                  });
-
-            column
-                  .data()
-                  .unique()
-                  .sort()
-                  .each(function (d, j) {
-                     select.append('<option value="' + d + '">' + d + '</option>');
-                  });
-         });
-      },
+      paging: true,
       "language": {
          "sProcessing":     "Procesando...",
          "sLengthMenu":     "Mostrar _MENU_ registros",
